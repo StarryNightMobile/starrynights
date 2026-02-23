a24f0f86b90a4771206486d39bfe660e291f02d2
StarryNightMobile <starrynight255@proton.me>
2026-02-23 02:21:38 +0000

    Ready for production

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ import adminRoutes from './routes/admin.js';
 import authRoutes from './routes/auth.js';
 
 dotenv.config();
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
 const app = express();
 const port = process.env.PORT || 3000;
 
